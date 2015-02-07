b524b9d6418ec83530410421568f1621520f7c3b
EdWard680 <ezeward4@gmail.com>
2015-02-06 21:47:32 -0500

    Reworks DataService interface for timeout management and failure case

--- src/DataService.cpp
+++ src/DataService.cpp
@@ -7,46 +7,63 @@
 
 #include "DataService.h"
 
-void DataService::LoggingThread(DataService * const ls)
+const DataService::DS_HANDLER DataService::Create(std::function<DataService *()> factory)
 {
+	DS_HANDLER ret(factory());
+	ret->initThread(ret);
+	return ret;
+}
 
-	if(ls == nullptr)
+void DataService::DataThread(DataService::DS_HANDLER ds)
+{
+	if(ds == nullptr)
 	{
 		return;
 	}
-	while(ls->getThreadState() == THREAD_STATE_INIT);
+	while(ds->getThreadState() == THREAD_STATE_INIT);
 	int failed = 0;
-	//clock_t c = 0;
-	while(ls->getThreadState() == THREAD_STATE_RUNNING && failed >= 0)
+	while(ds->getThreadState() == THREAD_STATE_RUNNING && failed >= 0)
 	{
-		//if(clock() - c >= (int)ls->getLogPeriod()*CLOCKS_PER_SEC/1000)
-		//{
-			failed = ls->LogAll();
-		//	c = clock();
-		//}
-		std::this_thread::sleep_for(std::chrono::milliseconds(ls->getLogPeriod()));
+		clock_t c = clock();
+		failed = ds->LogAll();
+
+		// Ensures logger keeps to schedule
+		if(clock() - c > ds->getLogPeriod()*CLOCKS_PER_SEC/1000)
+			std::this_thread::sleep_for(std::chrono::milliseconds(ds->getLogPeriod() - (clock()-c)*1000/CLOCKS_PER_SEC));
 	}
-    ls->LogAllCurrent();
-    if(ls->getThreadState() == THREAD_STATE_RUNNING)
+    ds->LogAllCurrent();
+    if(ds->getThreadState() == THREAD_STATE_RUNNING)
     	; // In which the logger quit due to a callback function returning KILL
+}
+
+DataService::DataService(const DataService& other):
+		logObjects(other.logObjects), logThread(),
+		threadState(THREAD_STATE_RUNNING), logPeriodMils(other.logPeriodMils), time(0), logIter(other.logIter < 0? -1:other.logIter+1)
+{
+
 }
 
 DataService::~DataService()
 {
 	joinThread();
-	for(auto i = logObjects.begin(); i != logObjects.end(); i++)
+	/*for(auto i = logObjects.begin(); i != logObjects.end(); i++)
     {
 		delete *i;
-    }
+    }*/
     logObjects.clear();
 }
 
 const int DataService::LogAll()
 {
 	int ret = 0;
-	for(unsigned int i = 0; i < logObjects.size() && ret >=0; i++)
-		ret |= logObjects[i]->Log();
+	feedTimeout();
+	for(logIter = 0; logIter < logObjects.size() && ret >=0; logIter++)
+	{
+		ret |= logObjects[logIter]->Log();
+		feedTimeout();
+	}
 
+	logIter = -1;
 	return ret;
 }
 
@@ -58,3 +75,36 @@ const int DataService::LogAllCurrent()
 
 	return ret;
 }
+
+void DataService::initThread(const DataService::DS_HANDLER& self)
+{
+	if(self != this)
+		return;
+
+	logThread = thread(&DataService::DataThread, self);
+}
+
+void DataService::killThread()
+{
+	stopThread();
+	logThread.detach();
+}
+
+
+const bool DataService::exceedsTimeout(unsigned int microseconds)
+{
+	if(logIter < 0)  // Not in the midst of logging, no chance of blocking, no need for time out
+		return false;
+
+	if(microseconds == 0) // Default case
+		microseconds = getLogPeriod()*1000/logObjects.size();
+
+
+	return time*1000*1000/CLOCKS_PER_SEC > microseconds;
+}
+
+const DataService::DS_HANDLER DataService::emergencyClone() const
+{
+	return Create(new DataService(*this));
+}
+
--- src/DataService.h
+++ src/DataService.h
@@ -8,6 +8,8 @@
 #ifndef SRC_DATASERVICE_H_
 #define SRC_DATASERVICE_H_
 
+#include <ctime>
+#include <memory>
 
 #include "Defaults.h"
 #include "Loggable.h"
@@ -19,6 +21,7 @@ using std::thread;
 using std::ofstream;
 using std::stringstream;
 using std::endl;
+using std::shared_ptr;
 
 
 /** Pure virtual base class for managing logging
@@ -27,21 +30,33 @@ using std::endl;
 
 class DataService
 {
+public:
+	typedef shared_ptr<DataService> DS_HANDLER;
 protected:
     enum {THREAD_STATE_INIT, THREAD_STATE_RUNNING, THREAD_STATE_TERMINATE};
 
 private:
-	static void LoggingThread(DataService * const ls);
+	static void DataThread(DS_HANDLER ds);
+
+public:
+	static const DS_HANDLER Create(std::function<DataService * ()> factory);
 
 private:
-	vector<Loggable *> logObjects;
+	vector<shared_ptr<Loggable>> logObjects;
 	thread logThread;
 	short threadState;
 	unsigned int logPeriodMils;
+	clock_t time;
+	int logIter;
+
+protected:  // Ensures dynamic allocation of DataServices
+	DataService(const bool start=false, const unsigned int period=0): logThread(),
+		threadState(start? THREAD_STATE_RUNNING:THREAD_STATE_INIT),
+		logPeriodMils(period), time(0), logIter(0) {};
+
+	DataService(const DataService& other);   /// Called by emergencyClone
 
 public:
-	DataService(const bool start=false, const unsigned int period=0):
-		logThread(&DataService::LoggingThread, this), threadState(start? THREAD_STATE_RUNNING:THREAD_STATE_INIT), logPeriodMils(period) {};
 	virtual ~DataService();
 
 protected:
@@ -49,17 +64,32 @@ protected:
     virtual const int LogAllCurrent();
 
 protected:
-    void appendLog(Loggable * const l) {if(l != nullptr) logObjects.push_back(l);};
+    void appendLog(Loggable * const l) {if(l != nullptr && threadState != THREAD_STATE_RUNNING) logObjects.push_back(shared_ptr<Loggable>(l));};
+
+private:
+    void initThread(const DS_HANDLER& self);
 
 protected:
     const char getThreadState() const {return threadState;};
-    void setThreadState(const char state) {threadState = state;};
-    void runThread() {setThreadState(THREAD_STATE_RUNNING);};
-    void stopThread() {setThreadState(THREAD_STATE_TERMINATE);};
-    void joinThread() {stopThread(); if(logThread.joinable()) logThread.join();};
+    void setThreadState(const short state) {if(logThread.joinable()) threadState = state;};  /// Changes safe thread state
+    void runThread() {setThreadState(THREAD_STATE_RUNNING);};	   /// Allows thread to start running
+    void stopThread() {setThreadState(THREAD_STATE_TERMINATE);};   /// Safely changes thread state to stop running
+    void killThread();	/// Detaches thread and sets state to stop running
+    void joinThread() {stopThread(); if(logThread.joinable()) logThread.join();};   /// Waits for thread to finish
+    void feedTimeout() {time = clock();};
+
+public:
+    /// Literally too lazy to reorganize the functions above with different access modifiers
+    void startLogging() {runThread();};
+    void stopLogging() {stopThread();};
+    void killLogging() {killThread();};
 
 public:
     const unsigned int getLogPeriod() const {return logPeriodMils;};
+    const short getThreadState() const {return threadState;};
+    const bool exceedsTimeout(unsigned int microseconds=0);   /// Returns true if a timeout has been exceeded in a state of logging. 0 indicates to use default timeout
+    const int getCurrentIteration() const {return logIter;};
+    virtual const DS_HANDLER emergencyClone() const;		/// Clones the DataService with all log objects except for the fail-point
 };
 
 
