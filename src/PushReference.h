db419798d6442c8c4564afc4644317ab59101c3b
EdWard680 <ezeward4@gmail.com>
2015-02-09 20:33:43 -0500

    Splits PushReference into PushValue and PushReference

--- src/PushReference.h
+++ src/PushReference.h
@@ -13,20 +13,18 @@
 #include <memory>
 
 #include "PollValue.h"
-#include "Pusher.h"
+#include "PushValue.h"
 
 namespace DRR
 {
-
-using std::mutex;
 using std::shared_ptr;
 using std::weak_ptr;
 
 template<typename T>
-class PushReference: public Pusher
+class PushReference: public PushValue<T>
 {
 public:
-	typedef std::function<void(T)> FUNC_t;
+	typedef typename PushValue<T>::FUNC_t FUNC_t;
 
 public:
 	static const shared_ptr<PushReference<T> > Create(const T* val, const FUNC_t& fn)
@@ -37,12 +35,10 @@ public:
 private:
 	const T *ref;	///< Pointer to data which is to be pushed
 	weak_ptr<PollValue<T> > refObj;	///< Weak pointer to the referring object
-	FUNC_t pushFunc; ///< Function called by push
-	mutex sem;		///< Mutex which keeps the pushFunc call from ever being called concurrently
 
 protected:
-	PushReference(const T *val, const FUNC_t fn): ref(val), refObj(nullptr), pushFunc(fn) {};
-	PushReference(const weak_ptr<PollValue<T> > obj, const FUNC_t fn): ref(&refObj->getPrevious()), refObj(obj), pushFunc(fn) {};
+	PushReference(const T *val, const FUNC_t fn): PushValue<T>(fn), ref(val), refObj(nullptr) {};
+	PushReference(const weak_ptr<PollValue<T> > obj, const FUNC_t fn): PushValue<T>(fn), ref(&refObj->getPrevious()), refObj(obj) {};
 	PushReference(const PushReference& other)=delete;
 
 public:
@@ -54,22 +50,12 @@ public:
 		if(refObj.expired() || refObj == nullptr || ref == NULL)	// Invalid states
 			return -1;
 
-		if(sem.try_lock())
-		{
-			try
-			{
-				pushFunc(*ref);
-			}
-			catch(...)
-			{
-				sem.unlock();
-				return -1;
-			}
+		return PushValue<T>::Push();
+	}
 
-			sem.unlock();
-		}
-
-		return 0;
+	virtual const T GetValue() override
+	{
+		return *ref;
 	}
 
 };
