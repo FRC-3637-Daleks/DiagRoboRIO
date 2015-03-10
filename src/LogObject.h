75cc1c9d0c767aeda752e9953361cf28e3c4c9ca
Elvin Kong <elvinkong97@gmail.com>
2015-03-10 15:45:46 -0400

    Removes CV-qualifier conversion AddLog function due to seg-faults

--- src/LogObject.h
+++ src/LogObject.h
@@ -38,12 +38,6 @@ public:
 		return LogService::AddLog(component, fn, self, dashData);
 	}
 
-	template<typename T>
-	const int AddLog(const string &component, const T (SERVICE::*fn)(), const int dashData=-1)
-	{
-		return AddLog<T>(component, fn, dashData);
-	}
-
 	template<typename T, class... Args>
 	const int AddLog(const string &component, const std::function<T(SERVICE*, Args...)> &fn, Args... args, const int dashData=-1)
 	{
