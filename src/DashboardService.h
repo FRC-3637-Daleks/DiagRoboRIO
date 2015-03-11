1a14beae201cdd5433cf969643f06d21f5d58252
EdWard680 <ezeward4@gmail.com>
2015-03-10 20:51:52 -0400

    Adds Text Logging to DashboardService

--- src/DashboardService.h
+++ src/DashboardService.h
@@ -13,6 +13,8 @@
 #include "DashboardPusher.h"
 #include "DiagnosticService.h"
 
+#include "TextLog.h"
+
 namespace DRR
 {
 
@@ -25,14 +27,20 @@ private:
 	string path;
 
 public:
-	DashboardService(const shared_ptr<DashboardPusher>& push, const string& p = string("")): pusher(push), path(p) {AddThread(pusher);};
+	DashboardService(const shared_ptr<DashboardPusher>& push, const string& p = string("")): pusher(push), path(p)
+	{
+		TextLog::Log("DashboardService")<<"Created for path: "<<path;
+		if(!AddThread(pusher))
+			TextLog::Log("DashboardService", LEVEL_t::ERR)<<"Could not Add a new thread for this pusher ("<<path<<")";
+	};
 
 public:
-	virtual ~DashboardService() {};
+	virtual ~DashboardService() {TextLog::Log("DashboardService", "Destroying");};
 
 public:
 	void SetPusher(const shared_ptr<DashboardPusher>& push)
 	{
+		TextLog::Log("Dashboard Service", "Installing New Pusher");
 		pusher = push;
 		AddThread(pusher);
 	}
@@ -41,16 +49,35 @@ public:
 
 public:
 	template<typename T>
-	const shared_ptr<PushReference<T>> Add(const Frame::KEY_t key, const weak_ptr<PollValue<T>>& poll, const int data=0)
+	const shared_ptr<PushReference<T>> Add(const Frame::KEY_t key, const weak_ptr<PollValue<T>>& poll, const int data=-1)
 	{
 		auto pushRef = pusher->Add(path.empty()? key:(path+'/'+key), poll, data);
+		if(pushRef == nullptr)
+		{
+			auto save = TextLog::Log("DashboardService", LEVEL_t::ERR)<<"Add(key: "<<key<<", poll: ";
+			auto pollLock = poll.lock();
+			if(pollLock == nullptr)
+				save<<="NULL";
+			else
+				save<<="non-null";
+			save<<", data: "<<data;
+		}
 		return pushRef;
 	}
 
 	template<typename T>
-	const shared_ptr<PushReference<T>> Add(const Frame::KEY_t key, const T *val, const int data=0)
+	const shared_ptr<PushReference<T>> Add(const Frame::KEY_t key, const T *val, const int data=-1)
 	{
 		auto pushRef = pusher->Add(path.empty()? key:(path+'/'+key), val, data);
+		if(pushRef == nullptr)
+		{
+			auto save = TextLog::Log("DashbaordService", LEVEL_t::ERR)<<"Add(key: "<<key<<", val: ";
+			if(val == NULL)
+				save<<="NULL";
+			else
+				save<<="non-null";
+			save<<", data: "<<data;
+		}
 		return pushRef;
 	}
 };
